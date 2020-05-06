853b40e4b9f14e77dd46f3dd9015cd729f720692
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-06 18:09:33 +0200

    wcf based cmdlet

---
+++ ProductivityTools.Learning.PSModuleFrameworksWCF/CmdletNetFramework48WCF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.Learning.PSModuleFrameworks.NetFramework48WCF
+{
+    [Cmdlet(VerbsDiagnostic.Test, "NetFramework48WCF")]
+    public class CmdletNetFramework48WCF : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            System.ServiceModel.ServiceHost host = new System.ServiceModel.ServiceHost(typeof(Service1));
+            WriteObject("Hello from NetFramework48WCF");
+        }
+    }
+
+    public class Service1 { }
+}
--- ScriptContainer/TestModules.ps1
+++ ScriptContainer/TestModules.ps1
@@ -1,7 +1,9 @@
 Import-Module .\ProductivityTools.Learning.PSModuleFrameworks.NetFramework48.dll
+Import-Module .\ProductivityTools.Learning.PSModuleFrameworks.NetFramework48WCF.dll
 Import-Module .\netcoreapp3.1\ProductivityTools.Learning.PSModuleFrameworks.NetCore31.dll
 Import-Module .\netstandard2.0\ProductivityTools.Learning.PSModuleFrameworks.NetStandard20.dll
 
 Test-NetCore31
-Test-NetFramework48
 Test-NetStandard20
+Test-NetFramework48
+Test-NetFramework48WCF
