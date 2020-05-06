e1075c992c6f6ccf554918d9d07cde8a0bfd0c3e
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-06 16:39:02 +0200

    netcore31

---
+++ ProductivityTools.Learning.PSModuleFrameworks.NetCore31/CmdletNetCore31.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace ProductivityTools.Learning.PSModuleFrameworks.NetCore31
+{
+    [Cmdlet(VerbsDiagnostic.Test, "NetCore31")]
+    public class CmdletNetCore31 : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject("Hello from NetCore31");
+        }
+    }
+}
--- ProductivityTools.Learning.PSModuleFrameworks.NetStandard/CmdletNetStandard.cs
+++ ProductivityTools.Learning.PSModuleFrameworks.NetStandard/CmdletNetStandard.cs
@@ -3,12 +3,12 @@ using System.Management.Automation;
 
 namespace ProductivityTools.Learning.PSModuleFrameworks.NetStandard20
 {
-   [Cmdlet(VerbsDiagnostic.Test, "NetFramework48")]
+   [Cmdlet(VerbsDiagnostic.Test, "NetStandard20")]
     public class CmdletCmdletNetStandard20 : PSCmdlet
     {
         protected override void ProcessRecord()
         {
-            WriteObject("Hello from NetFramework48");
+            WriteObject("Hello from NetStandard20");
         }
     }
 }
