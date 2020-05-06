37b4d9f646c42453cfc75146c83e785efcc0ae94
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-06 16:09:33 +0200

    net framework net standard

---
+++ ProductivityTools.Learning.PSModuleFrameworks.NetFramework48/CmdletNetFramework48.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.Learning.PSModuleFrameworks.NetFramework48
+{
+    [Cmdlet(VerbsDiagnostic.Test, "NetFramework48")]
+    public class CmdletNetFramework48 : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject("Hello from NetFramework48");
+        }
+    }
+}
---
+++ ProductivityTools.Learning.PSModuleFrameworks.NetStandard/CmdletNetStandard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace ProductivityTools.Learning.PSModuleFrameworks.NetStandard20
+{
+   [Cmdlet(VerbsDiagnostic.Test, "NetFramework48")]
+    public class CmdletCmdletNetStandard20 : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject("Hello from NetFramework48");
+        }
+    }
+}
